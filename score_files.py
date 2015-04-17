117cf05abdbf440bd1417bf2f3b7ef1aadb5460e
Fabio Machado <fabiohsmachado@gmail.com>
2015-04-17 04:01:07 -0300

    Pointed to local scorer, renamed ess parameter

--- score_files.py
+++ score_files.py
@@ -4,22 +4,22 @@ import sys
 import os
 from subprocess import call
 
-def ScoreDatasetFile(pathToScorer, pathToDataset, alpha, palim):
+def ScoreDatasetFile(pathToScorer, pathToDataset, ess, palim):
  print "Scoring the dataset", pathToDataset;
  scoreFileName = os.path.splitext(pathToDataset)[0] + ".scores";
- scoreCommand = [pathToScorer, pathToDataset, str(alpha), str(palim)];
+ scoreCommand = [pathToScorer, pathToDataset, str(ess), str(palim)];
  with open(scoreFileName, "w") as scoreFile:
   call(scoreCommand, stdout = scoreFile, shell = False);
  print "Finished scoring.";
  return scoreFileName;
 
 def Error():
- print "Usage:", sys.argv[0], "data_files", "alpha", "parents_limit";
+ print "Usage:", sys.argv[0], "data_files", "ess", "parents_limit";
  exit(0);
 
-def ScoreFiles(fileList, alpha, parentsLimit):
- pathToScorer = "/opt/bnet/learning/gobnilp-1.4.1-cplex/scoring"; 
- return [ScoreDatasetFile(pathToScorer, datasetFile, alpha, parentsLimit) for datasetFile in fileList if os.path.isfile(datasetFile)];
+def ScoreFiles(fileList, ess, parentsLimit):
+ pathToScorer = "/scorer/unix/scoring"; 
+ return [ScoreDatasetFile(pathToScorer, datasetFile, ess, parentsLimit) for datasetFile in fileList if os.path.isfile(datasetFile)];
  
 if __name__ == "__main__":
  try:
